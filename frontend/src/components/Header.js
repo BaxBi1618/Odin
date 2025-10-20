24cecc58943d5f41e676221aef935c0bc6ba3de9
Michał Garnys <m.programista@wp.pl>
2025-10-21 01:11:33 +0200

    add header and variables color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,20 +2,26 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
 
 function Header(){
     return(
-        <Navbar variant='dark' bg='secondary' sticky="top">
+        <Navbar variant='dark' bg='secondary' sticky="top" expand="sm">
             <Container fluid className='px-5'>
-                <Navbar.Brand href="/">Odin</Navbar.Brand>
-                <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-                >
-                    <Nav.Link href="/articles">Artykuły</Nav.Link>
-                </Nav>
-                <Button variant="primary">Login</Button>
+                <Navbar.Brand href="/" className="me-5">Odin</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav
+                    className="me-auto my-2 my-lg-0"
+                    navbarScroll
+                    >
+                        <Nav.Link as={NavLink} to="/articles" >Artykuły</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about" >O nas</Nav.Link>
+                        <Nav.Link as={NavLink} to="/contact" >Kontakt</Nav.Link>
+                    </Nav>
+                    <Button variant="primary" className='mx-2' href="/login">Zaloguj</Button>
+                    <Button variant="primary" className='mx-2' href="/register">Zarejestruj</Button>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
